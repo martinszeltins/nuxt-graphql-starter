24e563ba219a00e22a8e5c4dccb381325c80482e
Martins Zeltins <queeeeenz@gmail.com>
2024-03-19 07:35:37 +0200

    [master] Added gql.tada

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+
+    typescript: {
+        tsConfig: {
+            compilerOptions: {
+                plugins: [{
+                    "name": "@0no-co/graphqlsp",
+                    "schema": "https://graphqlzero.almansi.me/api",
+                    "tadaOutputLocation": "./graphql-env.d.ts"
+                }]
+            }
+        }
+    }
 })
